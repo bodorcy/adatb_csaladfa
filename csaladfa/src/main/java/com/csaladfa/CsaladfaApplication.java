aba05a74188767771cea16b79be63de6307f1f85
Vincze Nándor <h379409@stud.u-szeged.hu>
2024-11-14 13:43:37 +0100

    initialize project

---
+++ csaladfa/src/main/java/com/csaladfa/CsaladfaApplication.java
@@ -0,0 +1,13 @@
+package com.csaladfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaladfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsaladfaApplication.class, args);
+	}
+
+}
---
+++ csaladfa/src/test/java/com/csaladfa/CsaladfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaladfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaladfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
